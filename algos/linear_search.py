9834c088c8d7431b3445ab861495131774ca0c32
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2023-11-01 22:13:30 +0530

    Included a subroutine to compute time that has been elapsed

--- algos/linear_search.py
+++ algos/linear_search.py
@@ -1,9 +1,13 @@
+from timeit import default_timer as timer
+import random
+
 """
 The below is an implementation of the linear search algorithm. The linear search algorithm is an example of an O(n) time complexity algorithm. 
 The idea behind the linear search is that given an array and a key value, starting from the first element in the array, you traverse the array one by 
 one until you either reach the end of the array or you reach the element itself. In the event you reach the element, you return the index value at which 
 the value is held. 
 """
+
 
 def linear_search(arr,val):
     for i in range(len(arr)):
@@ -15,7 +19,16 @@ def linear_search(arr,val):
 
 # Main program begins here 
 
-arr1 = [2,3,4,5,6,7,8,8,8,9,9,9,110]
-ind_val = linear_search(arr1,111)
+
+arr1 = []
+for i in range(10000000):
+    arr1.append(random.randint(1,1000000))
+
+start = timer()
+ind_val = linear_search(arr1,20)
+end = timer()
+
+elapsed = (end - start)
+print(elapsed)
 
 print(ind_val)
