cb7e936987ab385d8a4503a95101063c2a0de9ab
Sandeep Chezhian A <sandeepchezhian@gmail.com>
2023-11-04 14:30:21 +0530

    Queues implementation is being added

---
+++ queues/queues.py
@@ -0,0 +1,10 @@
+"""
+    The below is an implementation of queues in the Python programming language. Queues are an abstract data type which has the following operations defined on them:
+        1) Enqueue (Key a) : Appends the key a to the end of the queue
+        2) Dequeue() : Removes the element at the front of the queue. 
+        3) Empty? : Returns a boolean value of True if the queue is empty and False otherwise.
+
+    Queues are an example of a FIFO data structure. (First In First Out).
+"""
+
+
